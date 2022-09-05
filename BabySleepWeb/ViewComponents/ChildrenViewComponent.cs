3a10a48472a65f4005dc624074117984f02e3175
kseniia <kszhuk@gmail.com>
2022-09-05 19:38:02 +0200

    Add new component for children

---
+++ BabySleepWeb/ViewComponents/ChildrenViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BabySleepWeb.ViewComponents
+{
+    [ViewComponent(Name = "Children")]
+    public class ChildrenViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var children = new List<SelectListItem>();
+
+            return View("Index", children);
+        }
+    }
+}
