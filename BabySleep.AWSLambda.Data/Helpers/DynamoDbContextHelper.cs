afae44fa6e54c83bbb1f1bbf293cc776a91fbec4
kseniia <kszhuk@gmail.com>
2022-10-04 11:28:24 +0200

    Add aws data app

---
+++ BabySleep.AWSLambda.Data/Helpers/DynamoDbContextHelper.cs
@@ -0,0 +1,23 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace BabySleep.AWSLambda.Data.Helpers
+{
+    internal class DynamoDbContextHelper
+    {
+        private static DynamoDBContext context;
+        public static DynamoDBContext GetDynamoDbContext()
+        {
+            if (context == null)
+            {
+                var clientConfig = new AmazonDynamoDBClient("AKIAYEEPPMAQHJZKUYHD", "1Bge87vU9hgLM0nmUaEfS9JC5+GcSdT6q163Zwu6",
+                    RegionEndpoint.EUWest1);
+
+                context = new DynamoDBContext(clientConfig);
+            }
+
+            return context;
+        }
+    }
+}
