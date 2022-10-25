8e4dbe6e7b0f34d17a917295d308801fdd25a1aa
kseniia <kszhuk@gmail.com>
2022-10-25 14:05:42 +0200

    Add calendar to Sleeps page

---
+++ BabySleepWeb/wwwroot/js/sleeps.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    $('#datepicker').datepicker({
+        autoclose: true,
+        format: "MM dd, yyyy",
+        immediateUpdates: true,
+        startDate: new Date(new Date().setDate(new Date().getDate() - 30)),  
+        endDate: new Date(new Date().setDate(new Date().getDate() + 1))
+    }).datepicker("setDate", new Date());
+
+});
+
+$("#prev").click(function (e) {
+    var dateNew = $('#datepicker').datepicker('getDate', '-1d');
+    dateNew.setDate(dateNew.getDate() - 1);
+    $('#datepicker').datepicker('setDate', dateNew);
+    e.preventDefault();
+});
+
+
+$("#next").click(function (e) {
+    var dateNew = $('#datepicker').datepicker('getDate', '+1d');
+    dateNew.setDate(dateNew.getDate() + 1);
+    if (cleanDate(dateNew) <= cleanDate(new Date())) {
+        $('#datepicker').datepicker('setDate', dateNew);
+    }
+    e.preventDefault();
+});
+
+function cleanDate(date) {
+    return date.setHours(0, 0, 0, 0);
+}
