e09cb69c14f26a1327d2c7afb7a5e90be2545a4f
kseniia <kszhuk@gmail.com>
2023-01-24 12:50:57 +0100

    Fill footer with info & style

--- BabySleepWeb/wwwroot/js/site.js
+++ BabySleepWeb/wwwroot/js/site.js
@@ -5,3 +5,11 @@
 function HideBusyIndicator() {
     document.getElementById("loading").style.display = "none";
 };
+
+$(document).ready(function() {
+    $('.dropdown-toggle').dropdown();
+});
+
+$(function() {
+    $('.selectpicker').selectpicker();
+});
